21ec5c89d95b4720ea64c4d70de837787b06ac1d
Asher <erxiao.tang@dal.ca>
2023-07-27 16:38:55 -0300

    Refactor: Bullet -> PlayerBullet & InvaderBullet

--- Space-invaders/app/src/main/java/com/example/space_invaders/InvaderBullet.kt
+++ Space-invaders/app/src/main/java/com/example/space_invaders/InvaderBullet.kt
@@ -1,50 +1,12 @@
 package com.example.space_invaders
 
-import android.graphics.RectF
+import android.graphics.Bitmap
 
-class InvaderBullet(screenY: Int,
-                    private val speed: Float = 350f,
-                    heightModifier: Float = 20f) {
-
-    val position = RectF()
-
-    // Which way is it shooting
-    val up = 0
-    val down = 1
-
-    // Going nowhere
-    private var heading = -1
-
-    private val width = 2
-    private var height = screenY / heightModifier
-
-    var isActive = false
-
-    fun shoot(startX: Float, startY: Float, direction: Int): Boolean {
-        if (!isActive) {
-            position.left = startX
-            position.top = startY
-            position.right = position.left + width
-            position.bottom = position.top + height
-            heading = direction
-            isActive = true
-            return true
-        }
-
-        // Bullet already active
-        return false
-    }
-
-    fun update(fps: Long) {
-
-        // Just move up or down
-        if (heading == up) {
-            position.top -= speed / fps
-        } else {
-            position.top += speed / fps
-        }
-
-        // Update the bottom position
-        position.bottom = position.top + height
-    }
-}
+/**
+ * Class for InvaderBullet
+ * @param screenY The vertical resolution of the screen
+ * @param speed The speed of the bullet
+ * @param heightModifier The height modifier of the bullet
+ */
+class InvaderBullet(screenY: Int, speed: Float = 350f, heightModifier: Float = 20f)
+    : Bullet(screenY, speed, heightModifier, width = 2)
--- Space-invaders/app/src/main/java/com/example/space_invaders/PlayerBullet.kt
+++ Space-invaders/app/src/main/java/com/example/space_invaders/PlayerBullet.kt
@@ -1,66 +1,36 @@
 package com.example.space_invaders
 
 import android.graphics.Bitmap
-import android.graphics.RectF
 
-class PlayerBullet(screenY: Int,
-                   private val speed: Float = 350f,
-                   heightModifier: Float = 20f,
-                   private val bulletBitmap: Bitmap // Single bitmap for bullet animation
-) {
-
-    val position = RectF()
-
-    // Animation frame properties
+class PlayerBullet(
+    screenY: Int,
+    speed: Float = 350f,
+    heightModifier: Float = 20f,
+    private var bulletBitmap: Bitmap
+) : Bullet(screenY, speed, heightModifier) {
     private var frameTime = 0L
     private val frameDuration = 100
 
-    // Which way is it shooting
-    val up = 0
-    val down = 1
-
-    // Going nowhere
-    private var heading = -1
-
-    private val width = 50
-    private var height = screenY / heightModifier
-
-    var isActive = false
-
-    fun shoot(startX: Float, startY: Float, direction: Int): Boolean {
-        if (!isActive) {
-            position.left = startX - 50
-            position.top = startY
-            position.right = position.left + width
-            position.bottom = position.top + height
-            heading = direction
-            isActive = true
-            return true
-        }
-
-        // Bullet already active
-        return false
-    }
-
-    fun update(fps: Long) {
+    /**
+     * Update the position of the bullet
+     * @param fps The frame rate of the game
+     */
+    override fun update(fps: Long) {
         // Check if the bullet animation should move to the next frame
         val currentTime = System.currentTimeMillis()
         if (currentTime > frameTime + frameDuration) {
             frameTime = currentTime
         }
 
-        // Move the bullet based on the current heading and speed
-        if (heading == up) {
-            position.top -= speed / fps
-        } else {
-            position.top += speed / fps
-        }
-        // Update the bottom position
-        position.bottom = position.top + height
+        // Call the update method of the super class (Bullet)
+        super.update(fps)
     }
 
+    /**
+     * Get the current frame of the bullet
+     * @return The current frame of the bullet
+     */
     fun getCurrentFrame(): Bitmap {
-        // Return the single bitmap for bullet animation
         return bulletBitmap
     }
 }
